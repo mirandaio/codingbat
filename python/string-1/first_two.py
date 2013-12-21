144aa3a2ae36b83089adf96c196ab75f29f955f8
Alfredo Miranda <alfredo@miranda.io>
2013-12-20 22:36:08 -0600

    Added first_two.py

---
+++ python/string-1/first_two.py
@@ -0,0 +1,6 @@
+# Given a string, return the string made of its first two chars, so the 
+# String "Hello" yields "He". If the string is shorter than length 2, return 
+# whatever there is, so "X" yields "X", and the empty string "" yields the 
+# empty string "".
+def first_two(str):
+    return str[:2]
