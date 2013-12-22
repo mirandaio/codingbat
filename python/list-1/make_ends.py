a3ae53200e153bc747d5ca3dc0945d7f2461423d
Alfredo Miranda <alfredo@miranda.io>
2013-12-22 13:00:12 -0600

    Added make_ends.py

---
+++ python/list-1/make_ends.py
@@ -0,0 +1,5 @@
+# Given an array of ints, return a new array length 2 containing the first and 
+# last elements from the original array. The original array will be length 1 
+# or more.
+def make_ends(nums):
+    return [nums[0], nums[-1]]
