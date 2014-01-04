3d1cbc7599ef70da21facadbbc3be50807f26b03
Alfredo Miranda <alfredo@miranda.io>
2014-01-03 21:04:47 -0600

    Completed lucky_sum.py

---
+++ python/logic-2/lucky_sum.py
@@ -0,0 +1,14 @@
+# Given 3 int values, a b c, return their sum. However, if one of the values 
+# is 13 then it does not count towards the sum and values to its right do not 
+# count. So for example, if b is 13, then both b and c do not count.
+def lucky_sum(a, b, c):
+    if a == 13:
+        return 0
+        
+    if b == 13:
+        return a
+                
+    if c == 13:
+        return a + b
+                          
+    return a + b + c
