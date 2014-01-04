0e1627feed5448b031e982f4902c5ee64e24076b
Alfredo Miranda <alfredo@miranda.io>
2014-01-03 19:19:46 -0600

    Some minor changes to sum67.java

--- java/array-2/sum67.java
+++ java/array-2/sum67.java
@@ -7,11 +7,11 @@ public int sum67(int[] nums) {
     boolean inRange = false;
       
     for(int i = 0; i < nums.length; i++) {
-        if(!inRange && nums[i] != 6)
-            sum += nums[i];
-                        
         if(nums[i] == 6)
             inRange = true;
+
+        if(!inRange)
+            sum += nums[i];
                                         
         if(inRange && nums[i] == 7)
             inRange = false;
