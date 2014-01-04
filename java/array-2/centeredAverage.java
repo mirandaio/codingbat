30b13659aebb3745ef777f8f214bc4ce0054f3ed
Alfredo Miranda <alfredo@miranda.io>
2014-01-03 18:41:01 -0600

    Improved centeredAverage.java

--- java/array-2/centeredAverage.java
+++ java/array-2/centeredAverage.java
@@ -7,41 +7,14 @@
  */
 public int centeredAverage(int[] nums) {
     int sum = 0;
-    int count = 0;
     int min = nums[0];
     int max = nums[0];
-    boolean foundMin = false;
-    boolean foundMax = false;
-              
-    for(int i = 1; i < nums.length; i++) {
+         
+    for(int i = 0; i < nums.length; i++) {
+        sum += nums[i];
         min = Math.min(min, nums[i]);
         max = Math.max(max, nums[i]);
     }
-                            
-    for(int i = 0; i < nums.length; i++) {
-        if(nums[i] != min && nums[i] != max) {
-            sum += nums[i];
-            count++;
-        }
-                                                      
-        if(nums[i] == min) {
-            if(foundMin) {
-                sum += nums[i];
-                count++;
-            } else {
-                foundMin = true;
-            }
-        }
-                                                                                                            
-        if(nums[i] == max) {
-            if(foundMax) {
-                sum += nums[i];
-                count++;
-            } else {
-                foundMax = true;
-            }
-        }
-    }
-                                                                                                                                                                  
-    return sum / count;
+                           
+    return (sum - min - max) / (nums.length - 2);
 }
