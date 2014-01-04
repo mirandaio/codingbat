d51bd971509467dae83a6cd0bb01a7c133841e99
Alfredo Miranda <alfredo@miranda.io>
2014-01-04 12:18:16 -0600

    Minor change to makeBricks.java

--- java/logic-2/makeBricks.java
+++ java/logic-2/makeBricks.java
@@ -3,7 +3,7 @@
  * if it is possible to make the goal by choosing from the given bricks.
  */
 public boolean makeBricks(int small, int big, int goal) {
-    int count = 5 * big > goal ? goal - (goal % 5) : 5 * big;
-      
-    return (goal - count) <= small;
+    int remainder = goal >= (5 * big) ? goal - (5 * big) : goal % 5;
+    
+    return small >= remainder;
 }
