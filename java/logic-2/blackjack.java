cfe8a7ef3e06c48be1d52ceaa8edd28b9f7822c9
desmochi <44042133+desmochi@users.noreply.github.com>
2019-10-03 14:56:31 -0700

    Alternative Way
    Here's another way to do this.

--- java/logic-2/blackjack.java
+++ java/logic-2/blackjack.java
@@ -2,14 +2,21 @@
  * without going over. Return 0 if they both go over.
  */
 public int blackjack(int a, int b) {
-    if(a > 21 && b > 21)
-        return 0;
-          
-    if(a > 21)
-        return b;
-                    
-    if(b > 21)
-        return a;
-                              
-    return a > b ? a : b;
+  if (a <= 21 && b > 21) {
+    return a;
+  }
+  else if (a > 21 && b <= 21) {
+    return b;
+  }
+  else if (a <= 21 && b <= 21) {
+    if (a > b) {
+      return a;
+    }
+    else {
+      return b;
+    }
+  }
+  else {
+    return 0;
+  }
 }
