852d7e1b9a3d7390aeefd4d509ac4bfa554963d3
desmochi <44042133+desmochi@users.noreply.github.com>
2019-10-03 15:05:57 -0700

    My way of doing this
    I know this is more complicated than the OP's way of doing it but by doing this I learned how you have to go from "macro to micro" when it comes to if and else if statements. If you were you put the first if statement into the last else if statement, the code would not work because it needs to check the statement with the "macro" condition, which is (a == b && b == c && a == c).

--- java/logic-2/makeBricks.java
+++ java/logic-2/makeBricks.java
@@ -2,8 +2,20 @@
  * of small bricks (1 inch each) and big bricks (5 inches each). Return true 
  * if it is possible to make the goal by choosing from the given bricks.
  */
-public boolean makeBricks(int small, int big, int goal) {
-    int remainder = goal >= (5 * big) ? goal - (5 * big) : goal % 5;
-    
-    return small >= remainder;
+public int loneSum(int a, int b, int c) {
+  if (a == b && b == c && a == c) {
+    return 0;
+  }
+  else if (a == b) {
+    return c;
+  }
+  else if (b == c) {
+    return a;
+  }
+  else if (a == c) {
+    return b;
+  }
+  else {
+    return a + b + c;
+  }
 }
